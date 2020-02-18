757e297a9cfff01841794af5ea8d5732bc594606
Owen Bezick <owbezick@davidson.edu>
2020-02-17 20:28:55 -0500

    Owen Create

---
+++ Owen/BOW.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Exploratory analysis of Virginia VS Purdue 2019 game closed captions.
+
+@author: owenbezick
+"""
+
+# read in .txt file
+f = open("VirginiaVSPurdue.txt", "r")
+
+# read file object as closedCaptions
+closedCaptions = f.read()
+
+# remove punctuation 
+import string # for string.puncutation
+table = str.maketrans('','', string.punctuation)
+stripped = closedCaptions.translate(table)
+
+# tokenize
+from nltk.tokenize import word_tokenize
+tokens = word_tokenize(stripped)
+
+# lowercase 
+lower_tokens = [t.lower() for t in tokens]
+
+# Get list of english stop words
+from nltk.corpus import stopwords
+stopWords = set(stopwords.words('english'))
+
+# Filter the stopwords out
+filtered_tokens = []
+for t in lower_tokens:
+    if t not in stopWords:
+        filtered_tokens.append(t)
+
+from nltk.stem import WordNetLemmatizer
+# Instantiate the WordNetLemmatizer
+wordnet_lemmatizer = WordNetLemmatizer()
+
+# Lemmatize all tokens into a new list: lemmatized
+lemmatized_tokens = [wordnet_lemmatizer.lemmatize(t) for t in filtered_tokens]
+
+from collections import Counter
+bow = Counter(lemmatized_tokens)
+
+print(bow.most_common(50))
