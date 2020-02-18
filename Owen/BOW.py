519b09b32543abd085247cae7f41a3c5060fb5c0
Dan Murphy <danielmurph8@gmail.com>
2020-02-18 00:50:58 -0500

    Added Dans work to README and added config files for subfolders within Dan

--- Owen/BOW.py
+++ Owen/BOW.py
@@ -7,7 +7,7 @@ Exploratory analysis of Virginia VS Purdue 2019 game closed captions.
 """
 
 # read in .txt file
-f = open("VirginiaVSPurdue.txt", "r")
+f = open("/Users/danielmurphy/Desktop/Stratifyd_IS/datasets/VirginiaVSPurdue2019CC.txt", "r")
 
 # read file object as closedCaptions
 closedCaptions = f.read()
@@ -16,32 +16,32 @@ closedCaptions = f.read()
 import string # for string.puncutation
 table = str.maketrans('','', string.punctuation)
 stripped = closedCaptions.translate(table)
+print(stripped)
+# # tokenize
+# from nltk.tokenize import word_tokenize
+# tokens = word_tokenize(stripped)
 
-# tokenize
-from nltk.tokenize import word_tokenize
-tokens = word_tokenize(stripped)
+# # lowercase 
+# lower_tokens = [t.lower() for t in tokens]
 
-# lowercase 
-lower_tokens = [t.lower() for t in tokens]
+# # Get list of english stop words
+# from nltk.corpus import stopwords
+# stopWords = set(stopwords.words('english'))
 
-# Get list of english stop words
-from nltk.corpus import stopwords
-stopWords = set(stopwords.words('english'))
+# # Filter the stopwords out
+# filtered_tokens = []
+# for t in lower_tokens:
+#     if t not in stopWords:
+#         filtered_tokens.append(t)
 
-# Filter the stopwords out
-filtered_tokens = []
-for t in lower_tokens:
-    if t not in stopWords:
-        filtered_tokens.append(t)
+# from nltk.stem import WordNetLemmatizer
+# # Instantiate the WordNetLemmatizer
+# wordnet_lemmatizer = WordNetLemmatizer()
 
-from nltk.stem import WordNetLemmatizer
-# Instantiate the WordNetLemmatizer
-wordnet_lemmatizer = WordNetLemmatizer()
+# # Lemmatize all tokens into a new list: lemmatized
+# lemmatized_tokens = [wordnet_lemmatizer.lemmatize(t) for t in filtered_tokens]
 
-# Lemmatize all tokens into a new list: lemmatized
-lemmatized_tokens = [wordnet_lemmatizer.lemmatize(t) for t in filtered_tokens]
+# from collections import Counter
+# bow = Counter(lemmatized_tokens)
 
-from collections import Counter
-bow = Counter(lemmatized_tokens)
-
-print(bow.most_common(100))
+# print(bow.most_common(100))
